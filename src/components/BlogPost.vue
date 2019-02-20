14bdf55073c33617e9762bef46408f828bd7a435
makozi <marizumakozi97@gmail.com>
2019-02-20 12:25:18 +0100

    updated READ.md

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -1,3 +1,24 @@
+<template>
+  <div class="section" id="blog-post">
+    <div class="container center">
+      <div class="has-text-centered">
+        <router-link v-if="post.meta.previous_post" :to="/blog/ + post.meta.previous_post.slug" class="button">
+          < {{ post.meta.previous_post.title }}
+        </router-link>
+        <router-link v-if="post.meta.next_post" :to="/blog/ + post.meta.next_post.slug" class="button">
+          {{ post.meta.next_post.title }} >
+        </router-link>
+      </div>
+      <hr>
+      <h1 class="is-size-2">{{ post.data.title }}</h1>
+      <h4>{{ post.data.author.first_name }} {{ post.data.author.last_name }}</h4>
+      <div class="content" v-html="post.data.body"></div>
+    </div>
+  </div>
+</template>
+
+
+
 <script>
   import { butter } from '@/buttercms'
   export default {
